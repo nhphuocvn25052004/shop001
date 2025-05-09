c5298ec9d6a669a004f7c6e09d8868f54954be75
phuoc.220214@tbd.edu.vn <phuoc.220214@tbd.edu.vn>
2025-05-09 16:41:41 +0000

    Edited admincp.menu_qlsp.menu_sp.add_danhmuc and admincp.menu_qlsp.menu_sp.add_danhmuc.delete_danhmuc

--- client_code/admincp/menu_qlsp/menu_sp/add_danhmuc/delete_danhmuc/__init__.py
+++ client_code/admincp/menu_qlsp/menu_sp/add_danhmuc/delete_danhmuc/__init__.py
@@ -3,27 +3,18 @@ from anvil import *
 import anvil.server
 
 class delete_danhmuc(delete_danhmucTemplate):
-  def __init__(self, **properties):
+  def __init__(self, row, **properties):
     self.init_components(**properties)
 
-    # Gọi server để lấy danh mục và nạp vào dropdown
-    self.ds_danh_muc = anvil.server.call('lay_tat_ca_danh_muc')
-    self.dropdown_1.items = [(row['tendanhmuc'], row) for row in self.ds_danh_muc]
+    self.row = row  # lưu bản ghi được truyền vào
 
-  def dropdown_1_change(self, **event_args):
-    # Khi chọn danh mục, hiển thị thông tin ra label
-    row = self.dropdown_1.selected_value
-    if row:
-      self.label_1.text = row['id_danhmuc']
-      self.label_2.text = row['tendanhmuc']
+    # Hiển thị thông tin
+    self.label_id.text = self.row['id_danhmuc']
+    self.label_ten.text = self.row['tendanhmuc']
 
-  def delete_btn_click(self, **event_args):
-    row = self.dropdown_1.selected_value
-    if row:
-      if confirm(f"Bạn có chắc muốn xoá danh mục '{row['tendanhmuc']}' không?"):
-        row.delete()
-        Notification("Đã xoá thành công!", timeout=2).show()
-        self.label_1.text = ""
-        self.label_2.text = ""
-        self.dropdown_1.items = [(r['tendanhmuc'], r) for r in anvil.server.call('lay_tat_ca_danh_muc')]
-        self.dropdown_1.selected_value = None
+  def xoa_btn_click(self, **event_args):
+    if confirm(f"Xoá danh mục '{self.row['tendanhmuc']}'?"):
+      self.row.delete()
+      Notification("Đã xoá thành công!", timeout=2).show()
+      self.raise_event("x-close-alert")  # Đóng popup sau khi xoá
+      self.raise_event("x-refresh")     # Yêu cầu làm mới danh sách nếu có
