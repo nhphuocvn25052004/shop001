6d54a52d1a3e922e4ad36b724a9dd7f5324faa4d
phuoc.220214@tbd.edu.vn <phuoc.220214@tbd.edu.vn>
2025-05-10 06:37:57 +0000

    Edited settings, sanpham_server.sanpham_server and admincp.menu_qlsp.menu_sp.add_sp

--- client_code/admincp/menu_qlsp/menu_sp/add_sp/__init__.py
+++ client_code/admincp/menu_qlsp/menu_sp/add_sp/__init__.py
@@ -1,27 +1,36 @@
 from ._anvil_designer import add_spTemplate
 from anvil import *
 import anvil.server
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.users
-
 
 class add_sp(add_spTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.tai_danh_sach_danh_muc()
 
-    # Any code you write here will run before the form opens.
+  def tai_danh_sach_danh_muc(self):
+    ds = anvil.server.call('lay_danh_sach_danh_muc')
+    self.danhsach_danhmuc.items = [(row['tendanhmuc'], row) for row in ds]
 
-  def danhsach_danhmuc_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
+  def chapnhan_click(self, **event_args):
+    ten_sp = self.ten_sanpham.text
+    gia_sp = self.gia_sanpham.text
+    danh_muc = self.danhsach_danhmuc.selected_value
+    hinh_anh = self.file_loader_1.file  # Nếu có ảnh
 
-  def gia_sanpham_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
+    if not ten_sp or not gia_sp or not danh_muc:
+      alert("Vui lòng nhập đầy đủ thông tin!", title="Thiếu thông tin")
+      return
 
-  def ten_sanpham_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
+    try:
+      kq = anvil.server.call('them_san_pham', ten_sp, gia_sp, danh_muc, hinh_anh)
+      alert(kq, title="Thành công")
+      self.ten_sanpham.text = ""
+      self.gia_sanpham.text = ""
+      self.danhsach_danhmuc.selected_value = None
+      self.upload_1.clear()
+    except Exception as e:
+      alert(f"Lỗi: {e}", title="Lỗi")
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
     pass
--- server_code/sanpham_server/sanpham_server.py
+++ server_code/sanpham_server/sanpham_server.py
@@ -1,18 +1,24 @@
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
+from anvil.tables import app_tables
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+@anvil.server.callable
+def lay_danh_sach_danh_muc():
+  return list(app_tables.tbl_danhmuc.search())
+
+@anvil.server.callable
+def them_san_pham(ten_san_pham, gia_san_pham, danh_muc_row, hinh_anh=None):
+  danh_sach = app_tables.tbl_sanpham.search()
+  id_list = [row['id_sanpham'] for row in danh_sach if row['id_sanpham'] is not None]
+  max_id = max(id_list) if id_list else 0
+  new_id = max_id + 1
+
+  app_tables.tbl_sanpham.add_row(
+    id_sanpham=new_id,
+    tensanpham=ten_san_pham,
+    giasanpham=gia_san_pham,
+    id_danhmuc=danh_muc_row['id_danhmuc'],
+    tendanhmuc=danh_muc_row['tendanhmuc'],
+    hinhanh=hinh_anh
+  )
+
+  return f"Đã thêm sản phẩm: {ten_san_pham} (ID: {new_id})"
