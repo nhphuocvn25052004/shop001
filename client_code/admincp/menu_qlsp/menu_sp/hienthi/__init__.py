51d2cd1212c53b56f7498125fb923d3c4a539285
phuoc.220214@tbd.edu.vn <phuoc.220214@tbd.edu.vn>
2025-05-10 16:12:12 +0000

    Edited admincp.menu_qlsp.menu_sp.hienthi and admincp.menu_qlsp.menu_sp

--- client_code/admincp/menu_qlsp/menu_sp/hienthi/__init__.py
+++ client_code/admincp/menu_qlsp/menu_sp/hienthi/__init__.py
@@ -7,10 +7,6 @@ class hienthi(hienthiTemplate):
 
     sp = self.item  # Mỗi item là 1 dòng từ bảng tbl_sanpham
 
-    # Gán dữ liệu
     self.Label_ten.text = sp['tensanpham']
     self.Label_gia.text = f"{sp['giasanpham']} VND"
-
-    # Hình ảnh nếu có
     self.Image_1.source = sp['hinhanh'] if sp['hinhanh'] else None
-    self.Image_1.visible = bool(sp['hinhanh'])
