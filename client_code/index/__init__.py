a17c4e53a89613bd2658aab92fd232a80ecb9d17
phuoc.220214@tbd.edu.vn <phuoc.220214@tbd.edu.vn>
2025-05-12 07:28:23 +0000

    Edited index and settings; Deleted admincp.dangky

--- client_code/index/__init__.py
+++ client_code/index/__init__.py
@@ -7,14 +7,27 @@ from anvil.tables import app_tables
 import anvil.users
 
 class index(indexTemplate):
-  def __init__(self, logged_in=False, **properties):
+  def __init__(self, **properties):
     self.init_components(**properties)
+    self.logged_in = False  # Mặc định chưa đăng nhập
+    self.update_ui()
+
+  def update_ui(self):
+    if self.logged_in:
+      self.dangxuat.text = "Đăng xuất"
+      self.sanpham.visible = True
+      self.banhang.visible = True
+    else:
+      self.dangxuat.text = "Đăng nhập"
+      self.sanpham.visible = False
+      self.banhang.visible = False
 
   def sanpham_click(self, **event_args):
     open_form('admincp.menu_qlsp.menu_sp')
 
   def dangxuat_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
+    self.logged_in = not self.logged_in  # Đảo trạng thái
+    self.update_ui()
+
 
   
