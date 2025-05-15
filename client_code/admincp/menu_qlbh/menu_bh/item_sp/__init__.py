575b9530cae4b5cdeebeb5a6a9053a8edf6fb1e8
phuoc.220214@tbd.edu.vn <phuoc.220214@tbd.edu.vn>
2025-05-15 14:47:43 +0000

    Edited theme.css, admincp.menu_qlbh.menu_bh and admincp.menu_qlbh.menu_bh.item_sp

--- client_code/admincp/menu_qlbh/menu_bh/item_sp/__init__.py
+++ client_code/admincp/menu_qlbh/menu_bh/item_sp/__init__.py
@@ -2,15 +2,18 @@ from ._anvil_designer import item_spTemplate
 from anvil import *
 
 class item_sp(item_spTemplate):
-  def __init__(self, item=None, **properties):  # ✅ nhận item
+  def __init__(self, item=None, **properties):
     self.init_components(**properties)
 
-    self.item = item or {}  # anvil.Row object hoặc dict
+    self.item = item or {}
 
-    self.label_tensp.text = self.item['tensanpham'] if 'tensanpham' in self.item else 'Không tên'
-    self.label_gia.text = f"{self.item['giasanpham']:,} VND" if 'giasanpham' in self.item else "0 VND"
-    self.image_1.source = self.item['hinhanh'] if 'hinhanh' in self.item and self.item['hinhanh'] else 'blank_image.png'
+    self.label_tensp.text = self.item.get('tensanpham', 'Không tên')
+    self.label_gia.text = f"{self.item.get('giasanpham', 0):,} VND"
+    self.image_1.source = self.item.get('hinhanh') or 'blank_image.png'
 
-    self.image_1.width = "80px"
-    self.image_1.height = "80px"
-    self.width = "120px"
+    self.image_1.width = "60px"
+    self.image_1.height = "60px"
+    self.image_1.align = 'center'
+    self.label_tensp.align = 'center'
+    self.label_gia.align = 'center'
+
